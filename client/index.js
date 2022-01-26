cf5873359c24a2af973924376286bbd8e947588e
Caz <carolinelouisa90@gmail.com>
2022-01-26 15:28:01 +0000

    added tea.html

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
 <script src = "/index.html" ></script>
+
+const resultsBtn = document.querySelector('#results-btn')
+const randomBtn = document.querySelector('#random-btn')
+
+resultsBtn.addEventListener('submit', submitHandler())
+
+function submitHandler(e) {
+    e.preventDefault()
+}
+
+function getData() {
+    fetch('http://localhost5000')
+    .then
+}
