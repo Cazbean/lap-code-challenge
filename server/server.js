00c692c5eb3ec023082b4f4a6e398b196734abcd
Caz <carolinelouisa90@gmail.com>
2022-01-26 11:59:05 +0000

    added more routes

--- server/server.js
+++ server/server.js
@@ -4,21 +4,28 @@ const app = express()
 const port = 5000
 const path = require('path');
 
-// Coffee results page 
-// const results = [
-//   {title: "Coffee - Wikipedia", content: "Coffee is a brewed drink prepared from roasted coffee beans, the seeds of berries from certain flowering plants in the Coffea genus."},
-//   {title: "Costa Coffee: The Nation\'s Favourite Coffee Shop", content: "Costa is the Nation's Favourite coffee shop and the largest and fastest growing coffee shop chain in the UK." },
-//   {title: "Coffee Beans | Blended Coffee | Whittard of Chelsea", content: "Whether you prefer whole coffee beans or freshly ground coffee, Whittard of Chelsea's selection of artisan coffees has something to suit your tastes."},
-// ];
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/results', (req, res) => {
-  res.sendFile(path.join(__dirname, '/results.html'))
+// Pages
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+
+app.get('/coffee', (req, res) => {
+  res.sendFile(path.join(__dirname, '/..client/coffee.html'))
+})
+
+app.get('/tea', (req, res) => {
+  res.sendFile(path.join(__dirname, '/tea.html'))
+})
+
+app.get('/hotchoc', (req, res) => {
+  res.sendFile(path.join(__dirname, '/hot-choc.html'))
+})
+
+app.get('/chailatte', (req, res) => {
+  res.sendFile(path.join(__dirname, '/chai-latte.html'))
 })
